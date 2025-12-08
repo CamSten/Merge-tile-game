fb5f914a0fc90e0aff59249ad9184a913e435802
CamSten <camilla.stenklov@gmail.com>
2025-12-08 13:27:57 +0100

    Initial commit

---
+++ src/GameComponents/Board.java
@@ -0,0 +1,6 @@
+package GameComponents;
+
+import javax.swing.*;
+
+public class Board extends JPanel {
+}
---
+++ src/GameComponents/Tile.java
@@ -0,0 +1,10 @@
+package GameComponents;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Tile extends JButton {
+    int value;
+    boolean empty;
+    Color color;
+}
