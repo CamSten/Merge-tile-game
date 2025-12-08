cce522c39d856563e23aaa2b2b3d25e09b384303
CamSten <camilla.stenklov@gmail.com>
2025-12-08 13:32:15 +0100

    Add skeleton for class Tile

--- src/GameComponents/Tile.java
+++ src/GameComponents/Tile.java
@@ -7,4 +7,32 @@ public class Tile extends JButton {
     int value;
     boolean empty;
     Color color;
+    int row;
+    int col;
+    public Tile(int value, boolean empty, Color color, int row, int col){
+        this.value = value;
+        this.empty = empty;
+        this.color = color;
+        this.row = row;
+        this.col = col;
+    }
+    public int getValue(){
+        return value;
+    }
+    public boolean isEmpty(){
+        return empty;
+    }
+    public Color color(){
+        return color;
+    }
+    public int getRow(){
+        return row;
+    }
+    public int getCol(){
+        return col;
+    }
+    public int[] getPosition(){
+        return new int[]{row, col};
+    }
+
 }
